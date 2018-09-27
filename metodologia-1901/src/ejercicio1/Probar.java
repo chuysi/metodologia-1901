af89d89fffcceb3e623ec6bccb877ebcc1336eba
T-107 <T-107@PC190>
2018-09-27 17:44:28 -0500

    ejercicio1

---
+++ metodologia-1901/src/ejercicio1/Probar.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1;
+
+/**
+ *
+ * @author T-107
+ */
+public class Probar {
+    public static void main(String[] args) {
+        Persona p=new Persona();
+        p.altura=1.65f;
+        p.peso=78;
+        Imc x=new Imc();
+        x.p=p;
+        x.calcular();
+        System.out.println("El valor del imc es "+x.valor);
+    }
+}
